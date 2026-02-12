ff7c4874193460d355d0f0a8b27b5459f37b77a1
Bangjing Yi <wwssd507@gmail.com>
2026-02-12 17:16:47 +0800

    完成 biz module 实现文件的上传 以及相对应的数据库操作

--- sMall-biz/src/main/java/com/spring/what/biz/controller/multishop/AttachFileGroupController.java
+++ sMall-biz/src/main/java/com/spring/what/biz/controller/multishop/AttachFileGroupController.java
@@ -1,11 +1,20 @@
 package com.spring.what.biz.controller.multishop;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.spring.what.biz.dto.AttachFileGroupDTO;
+import com.spring.what.biz.model.AttachFile;
+import com.spring.what.biz.model.AttachFileGroup;
+import com.spring.what.biz.service.AttachFileGroupService;
+import com.spring.what.biz.service.AttachFileService;
 import com.spring.what.biz.vo.AttachFileGroupVO;
 import com.spring.what.common.response.ServerResponseEntity;
+import com.spring.what.common.util.BeanUtil;
+import com.spring.what.security.AuthContext;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,28 +24,57 @@ import java.util.List;
 @Tag(name = "店铺-文件分组")
 public class AttachFileGroupController {
 
+    @Resource
+    private AttachFileGroupService attachFileGroupService;
+
+    @Resource
+    private AttachFileService attachFileService;
+
     @GetMapping("/list")
     @Operation(summary = "获取列表", description = "分页获取列表")
     public ServerResponseEntity<List<AttachFileGroupVO>> list() {
+        LambdaQueryWrapper<AttachFileGroup> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.eq(AttachFileGroup::getShopId, AuthContext.get().getTenantId());
+        List<AttachFileGroup> attachFileGroups = attachFileGroupService.list(lambdaQueryWrapper);
+        List<AttachFileGroupVO> attachFileGroupVOS = BeanUtil.mapAsList(attachFileGroups, AttachFileGroupVO.class);
+        return ServerResponseEntity.success(attachFileGroupVOS);
     }
 
     @GetMapping
     @Operation(summary = "获取", description = "根据attachFileGroupId获取")
     public ServerResponseEntity<AttachFileGroupVO> getByAttachFileGroupId(@RequestParam Long attachFileGroupId) {
+        AttachFileGroup attachFileGroup = attachFileGroupService.getById(attachFileGroupId);
+        AttachFileGroupVO attachFileGroupVO = BeanUtil.map(attachFileGroup, AttachFileGroupVO.class);
+        return ServerResponseEntity.success(attachFileGroupVO);
     }
 
     @PostMapping
     @Operation(summary = "保存", description = "保存")
     public ServerResponseEntity<Void> save(@Valid @RequestBody AttachFileGroupDTO attachFileGroupDTO) {
+        AttachFileGroup attachFile = BeanUtil.map(attachFileGroupDTO, AttachFileGroup.class);
+        attachFile.setAttachFileGroupId(null);
+        attachFile.setType(1);
+        attachFileGroupService.save(attachFile);
+        return ServerResponseEntity.success();
     }
 
     @PutMapping
     @Operation(summary = "更新", description = "更新")
     public ServerResponseEntity<Void> update(@Valid @RequestBody AttachFileGroupDTO attachFileGroupDTO) {
+        AttachFileGroup attachFileGroup = BeanUtil.map(attachFileGroupDTO, AttachFileGroup.class);
+        attachFileGroup.setShopId(AuthContext.get().getTenantId());
+        attachFileGroupService.updateById(attachFileGroup);
+        return ServerResponseEntity.success();
     }
 
     @DeleteMapping
     @Operation(summary = "删除", description = "根据id删除")
+    @Transactional(rollbackFor = Exception.class)
     public ServerResponseEntity<Void> delete(@RequestParam Long attachFileGroupId) {
+        LambdaQueryWrapper<AttachFile> attachFileLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        attachFileLambdaQueryWrapper.eq(AttachFile::getAttachFileGroupId, attachFileGroupId);
+        attachFileService.remove(attachFileLambdaQueryWrapper);
+        attachFileGroupService.removeById(attachFileGroupId);
+        return ServerResponseEntity.success();
     }
 }
